fe2b45865f9d4094fa561ce87045c7f2d9175877
John <nadenjohn@gmail.com>
2022-04-08 10:06:35 -0700

    Random quote works

--- stash_blog.rb
+++ stash_blog.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-class Herman
+class Herman    
+    QUOTES = ["That's my name! Don't wear it out!", "It's like you're trying to unravel a giant cable-knit sweater and someone keeps knitting and knitting.", "Because. I don't make monkeys, I just train 'em.", "There's a lotta things about me you don't know anything about, Dottie. Things you wouldn't understand. Things you couldn't understand. Things you shouldn't understand.", "I brought you guys... French fries!", " I'm a loner, Dottie. A rebel. So long, Dott."]
     attr_accessor :name
-
     def initialize(name)
         @name = name
     end 
     def quote
-        puts "That's my name! Don't wear it out!"
+        puts QUOTES.sample
     end
-end 
+end
 
-peewee = Herman.new("Pee Wee")
-peewee.quote
+pee_wee = Herman.new("Pee Wee")
+pee_wee.quote
